1894f0bda0bea65996cb173affd6e350f95f67b5
Cauan Roldao <cauanroldao1503@gmail.com>
2025-05-07 13:23:00 -0300

    diversas adições
    código de verificar se as cartas são iguais (match)e desabilitar eventlistener nelas, se são diferentes, e desvirar caso sejam diferentes. animação 3d para virar a carta usando css, ocultando o outro lado da parte de traz e mostrando e mostrando a parte da frente da carta

---
+++ script.js
@@ -0,0 +1,44 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        
+        return;
+    } 
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch();
+}
+
+
+function checkForMatch() {
+     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    
+     isMatch ? disableCards() : unflipCards();
+        disableCards();
+}
+
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);  
+}   
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
+    
+    
+}   
+
+cards.forEach(card => card.addEventListener('click', flipCard));
